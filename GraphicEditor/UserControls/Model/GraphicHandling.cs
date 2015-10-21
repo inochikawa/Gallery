481e696f46fae2c7b11a177f446e2c1c5ab3d15d
msCube <max.stecenko.max@gmail.com>
2015-10-22 02:59:19 +0300

    Revert "Fix <import> StyleCop settings"
    This reverts commit 36f496ad58152ed08a58b094038211098f2ebcc3.

--- GraphicEditor/UserControls/Model/GraphicHandling.cs
+++ GraphicEditor/UserControls/Model/GraphicHandling.cs
@@ -1,8 +1,5 @@
 ﻿namespace GraphicEditor.UserControls.Model
 {
-    /// <summary>
-    /// Main class where do all process of graphic editor
-    /// </summary>
     public class GraphicHandling
     {
         ////private PictureProxy f_picture;
